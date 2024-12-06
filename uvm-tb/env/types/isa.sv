0309ed48fb0d5f879e5e6e44d5719b97efa74d0e
R. Ignacio Genovese <roberto.genovese@bsc.es>
2024-12-06 21:09:41 +0100

    Updated ISS structures according to Spike

--- uvm-tb/env/types/isa.sv
+++ uvm-tb/env/types/isa.sv
@@ -1,50 +1,42 @@
-//typedef longint unsigned vec_els_t [MAX_64BIT_BLOCKS-1:0];
-//typedef longint unsigned mem_addrs_t [MAX_VLEN/MIN_SEW-1:0];
-//typedef longint unsigned mem_elements_t [MAX_VLEN/MIN_SEW-1:0];
-
-//typedef struct
-//{
-//    vec_els_t old_vd;
-//    vec_els_t vd;
-//    vec_els_t vs1;
-//    vec_els_t vs2;
-//    vec_els_t vs3;
-//    vec_els_t vmask;
-//} rvv_ops_t;
-
 typedef struct {
-    byte unsigned       frm;
-    byte unsigned       fflags;
-    byte unsigned       trap_illegal;
-    int unsigned        vstart;
-    int unsigned        vl;
-    int unsigned        vxrm;
-    int unsigned        vlmul;
-    int unsigned        vsew;
-    int unsigned        vill;
-    int unsigned        vxsat;
+    uint8_t  trap_illegal;
+    uint64_t mcause;
+    uint64_t scause;
+    uint32_t vstart;
+    uint32_t vl;
+    uint8_t  vxrm;
+    uint8_t  vlmul;
+    uint8_t  vsew;
+    uint8_t  vill;
+    uint8_t  vxsat;
+    uint8_t  vta;
+    uint8_t  vma;
+    uint8_t  frm;
+    uint8_t  fflags;
+    uint64_t mstatus;
+    uint64_t misa;
 } csr_t;
 
-//[TOCHECK] which fields are present in spike structure
 typedef struct {
-    longint unsigned    core_id;
-    longint unsigned    pc;
-    longint unsigned    instr;
-    longint unsigned    dst_value;
-    longint unsigned    dst_num;
-    longint unsigned    src1_value;
-    longint unsigned    src1_num;
-    longint unsigned    src2_value;
-    longint unsigned    src2_num;
-    string              disasm;
-    csr_t               csr;
-    byte unsigned       exc_bit;
-    longint unsigned    mem_addr;
-    longint unsigned    stored_value;
-    longint unsigned    destination_reg;
-    //rvv_ops_t           rvv_operands;
-    //mem_elements_t      mem_elements;
-    //mem_addrs_t         mem_addrs;
+    uint64_t core_id;
+    uint64_t pc;
+    uint32_t instr;
+    string   disasm;
+    uint8_t  dst_valid;
+    uint8_t  dst_num;
+    uint64_t dst_value;
+    uint8_t  src1_valid;
+    uint8_t  src1_num;
+    uint64_t src1_value;
+    uint8_t  src2_valid;
+    uint8_t  src2_num;
+    uint64_t src2_value;
+    uint64_t vaddr;
+    uint64_t paddr;
+    uint64_t store_data;
+    uint64_t store_mask;
+    csr_t    csr;
+    uint8_t  exc_bit;
 } iss_state_t;
 
 typedef struct {
